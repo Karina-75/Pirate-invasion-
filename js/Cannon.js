0dcc900f88d0d7bde131dfa2772567329924429d
Karina-75 <110104516+Karina-75@users.noreply.github.com>
2022-09-18 17:50:48 +0530

    Add files via upload

--- js/Cannon.js
+++ js/Cannon.js
@@ -5,16 +5,23 @@ class Cannon {
     this.width = width;
     this.height = height;
     this.angle = angle;
-    this.cannon_img = loadImage("assets/canon.png");
-    this.cannonBase_img = loadImage("assets/cannonBase.png");
-
+    this.cannon_image = loadImage("assets/canon.png");
+    this.cannon_base = loadImage("assets/cannonBase.png");
   }
   display() {
+    if (keyIsDown(RIGHT_ARROW) && this.angle < 70) {
+      this.angle = this.angle + 1;
+    }
+    if (keyIsDown(LEFT_ARROW) && this.angle > - 30) {
+      this.angle = this.angle - 1;
+    }
     push();
-    imageMode(CENTER)
-    image(this.cannon_img, this.x, this.y, this.width, this.height)
+    translate(this.x, this.y);
+    rotate(this.angle);
+    imageMode(CENTER);
+    image(this.cannon_image, 0, 0, this.width, this.height);
     pop();
-    image(this.cannonBase_img, 70, 20, 200, 200);
-    noFill()
+    image(this.cannon_base, 70, 20, 200, 200);
+    noFill();
   }
 }
